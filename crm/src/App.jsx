ed73bd3f31cdd400342377811cb1472de05660a9
richard murrray <murray571a@gmail.com>
2024-04-11 12:49:22 -0500

    login form

--- crm/src/App.jsx
+++ crm/src/App.jsx
@@ -1,16 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Login from './components/login/login'
+import Homepage from './components/homepage/homepage';
+import User from './components/user/user';
 
 function App() {
+  const[loggedIn, setLoggedIn] = useState()
 
 
   return (
-    <div>
-      <Login/>
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn}/>}/>
+          <Route path="/user" element={loggedIn == true ? <User/>: <Homepage/>}/>
+
+        </Route>
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
 
--- crm/src/components/login/login.jsx
+++ crm/src/components/login/login.jsx
@@ -1,18 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import '../login/login.css'
+import { useNavigate } from "react-router-dom"
 
 export default function (props) {
+  const [username, setUsername] = useState("username");
+  const[password, setPassword] =  useState("password");
+
+  const[usernameInput, setUsernameInput] = useState("");
+  const[passwordInput, setPasswordInput] = useState("")
+  let navigate = useNavigate();
+
+  const submit = (e) => {
+    console.log(username)
+    console.log(usernameInput)
+
+    console.log(password)
+    console.log(passwordInput)
+    e.preventDefault();
+    setUsername("username");
+    setPassword("password");
+
+    if(password === passwordInput && username == usernameInput){
+      props.setLoggedIn(true)
+      navigate('/user');
+    }else{
+      
+    }
+
+  }
+
   return (
     <div className="Auth-form-container">
-      <form className="Auth-form">
+      <form className="Auth-form"  onSubmit={(e) => submit(e)}>
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
           <div className="form-group mt-3">
-            <label>Email address</label>
+            <label>Username</label>
             <input
-              type="email"
+              type="text"
               className="form-control mt-1"
-              placeholder="Enter email"
+              placeholder="Enter Username"
+              onChange={(e) => setUsernameInput(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -21,7 +49,10 @@ export default function (props) {
               type="password"
               className="form-control mt-1"
               placeholder="Enter password"
+              onChange={(e) => setPasswordInput(e.target.value)}
+              
             />
+            <p>{passwordInput}</p>
           </div>
           <div className="d-grid gap-2 mt-3">
             <button type="submit" className="btn btn-primary">
