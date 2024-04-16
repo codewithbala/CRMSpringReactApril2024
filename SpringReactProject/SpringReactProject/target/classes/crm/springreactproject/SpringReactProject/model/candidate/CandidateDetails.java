1cb005f756a6779e65de76fe94e20e12774bf777
Yesahk <151057533+Yesahk@users.noreply.github.com>
2024-04-16 16:50:59 -0400

    Add files via upload
    Entity/Model for Candidate Details

---
+++ SpringReactProject/SpringReactProject/target/classes/crm/springreactproject/SpringReactProject/model/candidate/CandidateDetails.java
@@ -0,0 +1,71 @@
+package crm.springreactproject.SpringReactProject.model.candidate;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.*;
+
+@Entity
+@Table(name="candidate_details")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CandidateDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    private String recruiterName;
+
+    private Integer candidateEntry;
+
+    private String skillSet;
+
+    private Integer batchNo;
+
+    private String candidateStatus;
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private String emailId;
+
+    private String phoneNumber;
+
+    private String college;
+
+    private String country;
+
+    private String state;
+
+    private String city;
+
+    private String experience;
+
+    private String visaStatus;
+
+    private String OPTStartDate;
+
+    private String OPTEndDate;
+
+    private String SSN;
+
+    private String source;
+
+    private Integer communicationSkill;
+
+    private String resume;
+
+    private String recruiterRemarks;
+
+
+
+
+}
