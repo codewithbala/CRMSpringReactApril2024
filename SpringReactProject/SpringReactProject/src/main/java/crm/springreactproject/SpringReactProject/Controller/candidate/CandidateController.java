fbb2d44d463a66ed88649086aba0fe680acb2c5b
Yesahk <151057533+Yesahk@users.noreply.github.com>
2024-04-16 16:38:13 -0400

    Add files via upload
    Candidate Details Controller

---
+++ SpringReactProject/SpringReactProject/src/main/java/crm/springreactproject/SpringReactProject/Controller/candidate/CandidateController.java
@@ -0,0 +1,60 @@
+package crm.springreactproject.SpringReactProject.Controller.candidate;
+
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import crm.springreactproject.SpringReactProject.model.candidate.CandidateDetails;
+import crm.springreactproject.SpringReactProject.Service.candidate.Candidate;
+import java.util.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+@RestController
+@RequestMapping("/candidates")
+@CrossOrigin(origins = "http://localhost:4200")
+public class CandidateController {
+
+    @Autowired
+    private Candidate candidateService;
+
+    @CrossOrigin(origins="http://localhost:4200")
+    @GetMapping
+    public List<CandidateDetails> getAllCandidates() {
+        return candidateService.fetchAllCandidate();
+    }
+
+    @CrossOrigin(origins="http://localhost:4200")
+    @GetMapping("/{candidateId}")
+    public CandidateDetails getCandidateById(@PathVariable Integer candidateId) {
+        return candidateService.fetchCandidateById(candidateId);
+    }
+
+    @PostMapping
+    public CandidateDetails createCandidate(@RequestBody CandidateDetails candidateDetails) {
+        return candidateService.createNewCandidate(candidateDetails);
+    }
+
+    @CrossOrigin(origins="http://localhost:4200")
+    @DeleteMapping("/{candidateId}")
+    public ResponseEntity<String> deleteCandidate(@PathVariable Integer candidateId, CandidateDetails candidateDetails) {
+        candidateService.deleteCandidate(candidateId, candidateDetails);
+        return ResponseEntity.ok("Deleted Successfully");
+    }
+
+    @PutMapping("/{candidateId}")
+    public CandidateDetails updateCandidate(@PathVariable Integer candidateId, @RequestBody CandidateDetails candidateDetails) {
+        return candidateService.updateCandidate(candidateId, candidateDetails);
+
+
+    }
+
+}
