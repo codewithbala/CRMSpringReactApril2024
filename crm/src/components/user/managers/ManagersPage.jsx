9b6a32abf3e6862cbad7eb35c1ed32ea27cd78e1
richardisaninja2 <95777257+richardisaninja2@users.noreply.github.com>
2024-04-17 02:00:35 -0500

    changes

--- crm/src/components/user/managers/ManagersPage.jsx
+++ crm/src/components/user/managers/ManagersPage.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 export default function ManagersPage(props){
     const [managerId, setManagerId] = useState("");
     const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")))
-    const[employees, setEmployees] = useState([])
+    const[guys, setGuys] = useState([])
     const [link,setLink] = useState("");
     let baseUrl = "http://localhost:8080/api"
 
@@ -18,17 +18,17 @@ export default function ManagersPage(props){
     // console.log(user.hr_manager_id)
     let data ;
 
-      function getAllEmployees(){
+      const  getAllEmployees = () => {
         axios.get(link).then(response => {
-          setEmployees(response.data)
+          setGuys(response.data)
           console.log(response.data)
-          console.log(`data${employees}`)
+          console.log(`data${guys}`)
         }).catch(error => {
-          console.log(error.message)
+          console.log(error)
         })
       }
 
-
+  console.log(guys)
     useEffect(() => {
         if(department == "hr"){
           setLink(`${baseUrl}/hr/`)
@@ -43,8 +43,8 @@ export default function ManagersPage(props){
         }
             getAllEmployees();
 
-        console.log(employees)
-    },[managerId, user, link])
+        
+    },[managerId, user])
 
 
 
@@ -57,63 +57,68 @@ export default function ManagersPage(props){
         localStorage.removeItem("user")
         navigate("/");
       }
-    
-      return (
-        <div className="container mt-4">
-          <h1>Hello {user.firstName}</h1>
 
-          <table className="table">
-            <thead>
-              <tr>
-                <th>{department}_employee_id</th>
-
-                <th>birthdate</th>
-
-                <th>DepartmentId</th>
+      // if(guys?.length){
+      //   return(<p>Waiting on information</p>)
+      // }else{
+        return (
+          <div className="container mt-4">
+            <h1>Hello {user.firstName}</h1>
   
-                <th>Ed Level</th>
-
-                <th>Hire Date</th>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>{department}_employee_id</th>
+  
+                  <th>birthdate</th>
+  
+                  <th>DepartmentId</th>
+    
+                  <th>Ed Level</th>
+  
+                  <th>Hire Date</th>
+         
+                  <th>Position</th>
+                
+      
+                  <th>First Name</th>
+                
+                  <th>Last Name</th>
+                
+          
+                  <th>Phone Number</th>
+          
+  
+                  <th>Salary</th>
+    
        
-                <th>Position</th>
-              
-    
-                <th>First Name</th>
-              
-                <th>Last Name</th>
-              
-        
-                <th>Phone Number</th>
-        
-
-                <th>Salary</th>
-  
-     
-                <th>Gender</th>
-          </tr>
-            </thead>
-          </table>
-
-          <tbody>
-          {employees.map((employee, index) => (
-            <tr key={index}>
-              <td>{employee.employee_id}</td>
-              <td>{employee.birthdate}</td>
-              <td>{employee.departmentId}</td>
-              <td>{employee.edLevel}</td>
-              <td>{employee.hireDate}</td>
-              <td>{employee.position}</td>
-              <td>{employee.firstName}</td>
-              <td>{employee.lastName}</td>
-              <td>{employee.phoneNumber}</td>
-              <td>{employee.salary}</td>
-              <td>{employee.gender}</td>
+                  <th>Gender</th>
             </tr>
-          ))}
-        </tbody>
-
-          <button onClick={() => click()}>Create Business Employee</button>
-          <button onClick={() => logout()}>Logout</button>
-        </div>
-      );
+              </thead>
+            </table>
+  
+            <tbody>
+            {/* {guys?.map((employee, index) => (
+              <tr key={index}>
+                <td>{employee.birthdate}</td>
+                <td>{employee.departmentId}</td>
+                <td>{employee.edLevel}</td>
+                <td>{employee.hireDate}</td>
+                <td>{employee.position}</td>
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
+                <td>{employee.phoneNumber}</td>
+                <td>{employee.salary}</td>
+                <td>{employee.gender}</td>
+              </tr>
+            ))} */}
+          </tbody>
+  
+            <button onClick={() => click()}>Create Business Employee</button>
+            <button onClick={() => logout()}>Logout</button>
+          </div>
+        );
+      // }
+    
+      
 }
