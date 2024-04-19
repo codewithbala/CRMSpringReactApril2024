6da6b5aa144f412af6bf82d627d03880864aed23
richardisaninja2 <95777257+richardisaninja2@users.noreply.github.com>
2024-04-18 22:27:16 -0500

    added cors to candidate interview controller

--- SpringReactProject/SpringReactProject/src/main/java/crm/springreactproject/SpringReactProject/Controller/candidate/CandidateController.java
+++ SpringReactProject/SpringReactProject/src/main/java/crm/springreactproject/SpringReactProject/Controller/candidate/CandidateController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/candidates")
 public class CandidateController {
 
