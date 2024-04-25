853dc2a57baafff7e0e94c8a1252f388d7342b4f
richardisaninja2 <95777257+richardisaninja2@users.noreply.github.com>
2024-04-24 22:20:50 -0500

    deleted candidate interview class and classes corresponding to it

--- SpringReactProject/SpringReactProject/src/main/java/crm/springreactproject/SpringReactProject/Repository/interview/CandidateInterviewRepo.java
+++
@@ -1,7 +0,0 @@
-package crm.springreactproject.SpringReactProject.Repository.interview;
-
-import crm.springreactproject.SpringReactProject.model.interview.CandidateInterview;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateInterviewRepo extends JpaRepository<CandidateInterview, Long> {
-}
